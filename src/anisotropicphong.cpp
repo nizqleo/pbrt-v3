8bd4736df1d2841d4e63b16e8e0a17c0e32a3c2a
Ziqian Ni <39485337+nizqleo@users.noreply.github.com>
2019-11-27 20:36:52 +0000

    Add files via upload

---
+++ src/anisotropicphong.cpp
@@ -0,0 +1,78 @@
+
+/*
+    pbrt source code is Copyright(c) 1998-2016
+                        Matt Pharr, Greg Humphreys, and Wenzel Jakob.
+
+    This file is part of pbrt.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+    - Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    - Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+
+// materials/anisotropicphong.cpp*
+#include "materials/anisotropicphong.h"
+#include "paramset.h"
+#include "reflection.h"
+#include "interaction.h"
+#include "texture.h"
+#include "interaction.h"
+
+namespace pbrt {
+
+// AnisotropicPhongMaterial Method Definitions
+void AnisotropicPhongMaterial::ComputeScatteringFunctions(SurfaceInteraction *si,
+                                               MemoryArena &arena,
+                                               TransportMode mode,
+                                               bool allowMultipleLobes) const {
+
+    // Perform bump mapping with _bumpMap_, if present
+    if (bumpMap) Bump(bumpMap, si);
+    Spectrum Rd = RD->Evaluate(*si).Clamp();
+    Spectrum Rs = RS->Evaluate(*si).Clamp();
+    Float Nu = NU->Evaluate(*si);//.Clamp();
+    Float Nv = NV->Evaluate(*si);//.Clamp();
+
+    BxDF *spec = ARENA_ALLOC(arena, AnosotropicPhongBxDF)(*si, Rd, Rs, Nu, Nv);
+    // BxDF *spec =
+            
+    //assert(spec != NULL);
+    si->bsdf = ARENA_ALLOC(arena, BSDF)(*si);
+    si->bsdf->Add(spec);
+}
+
+AnisotropicPhongMaterial *CreateAnisotropicPhongMaterial(const TextureParams &mp) {
+    std::shared_ptr<Texture<Spectrum>> Rd =
+        mp.GetSpectrumTexture("Rd", Spectrum(0.01f));
+    std::shared_ptr<Texture<Spectrum>> Rs =
+        mp.GetSpectrumTexture("Rs", Spectrum(0.5f));
+        
+    std::shared_ptr<Texture<Float>> NV = mp.GetFloatTexture("nv", 10.f);
+    std::shared_ptr<Texture<Float>> NU = mp.GetFloatTexture("nu", 10000.f);
+    std::shared_ptr<Texture<Float>> bumpMap =
+        mp.GetFloatTextureOrNull("bumpmap");
+    return new AnisotropicPhongMaterial(Rd, Rs, NV, NU, bumpMap);
+}
+
+}  // namespace pbrt
